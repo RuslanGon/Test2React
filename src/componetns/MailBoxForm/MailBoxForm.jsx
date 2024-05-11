e09eca46fa2991b283d4d8faa679c3471d244b48
RuslanGon <ruslan441006@gmail.com>
2024-05-11 09:49:11 +0300

    wer

--- src/componetns/MailBoxForm/MailBoxForm.jsx
+++ src/componetns/MailBoxForm/MailBoxForm.jsx
@@ -1,4 +1,11 @@
-import { Field, Form, Formik } from "formik";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from 'yup';
+
+
+const formSchema = Yup.object({
+  userEmail: Yup.string().required('Enail adress must be').email('You must enter email'),
+  userName: Yup.string().required('Name is reguired').max(38, 'your user neme can be 38 simbol')
+});
 
 const INITIAL_VALUE = {
   userEmail: "",
@@ -27,6 +34,7 @@ const MailBoxForm = ({ onAddUser }) => {
   return (
     <Formik
       initialValues={INITIAL_VALUE}
+      validationSchema={formSchema}
       onSubmit={handleSubmit}>
       <Form>
         <h2>Add new user</h2>
@@ -34,12 +42,14 @@ const MailBoxForm = ({ onAddUser }) => {
           <span>user email</span>
           <br />
           <Field type="email" name="userEmail" placeholder="email" />
+          <ErrorMessage component='p' name="userEmail" />
         </label>
         <br />
         <label>
           <span>user name</span>
           <br />
           <Field type="name" name="userName" placeholder="name" />
+          <ErrorMessage component='p' name="userName" />
         </label>
         <br />
         <button type="submit">new contact</button>
