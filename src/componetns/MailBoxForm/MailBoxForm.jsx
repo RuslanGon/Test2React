f31bc3ee8eaec2f29d780c5083a349dcc900258a
RuslanGon <ruslan441006@gmail.com>
2024-05-12 11:28:48 +0300

    wer

--- src/componetns/MailBoxForm/MailBoxForm.jsx
+++ src/componetns/MailBoxForm/MailBoxForm.jsx
@@ -4,12 +4,14 @@ import * as Yup from 'yup';
 
 const formSchema = Yup.object({
   userEmail: Yup.string().required('Enail adress must be').email('You must enter email'),
-  userName: Yup.string().required('Name is reguired').max(38, 'your user neme can be 38 simbol')
+  userName: Yup.string().required('Name is reguired').max(38, 'your user neme can be 38 simbol'),
+  favColor: Yup.string().required('color is reguired').oneOf(['red', 'green', 'blue'])
 });
 
 const INITIAL_VALUE = {
   userEmail: "",
   userName: "",
+  favColor: ''
 }
 
 const MailBoxForm = ({ onAddUser }) => {
@@ -33,28 +35,44 @@ const MailBoxForm = ({ onAddUser }) => {
 
   return (
     <Formik
-      initialValues={INITIAL_VALUE}
-      validationSchema={formSchema}
-      onSubmit={handleSubmit}>
-      <Form>
-        <h2>Add new user</h2>
-        <label>
-          <span>user email</span>
-          <br />
-          <Field type="email" name="userEmail" placeholder="email" />
-          <ErrorMessage component='p' name="userEmail" />
-        </label>
+    initialValues={INITIAL_VALUE}
+    onSubmit={handleSubmit}
+    validationSchema={formSchema} >
+    <Form>
+      <h2>Add new users</h2>
+      <label>
+        <span>User email</span>
+        <br />
+        <Field type="email" name="userEmail" placeholder="@email" />
+        <ErrorMessage component="p" name="userEmail" />
+      </label>
+      <br />
+      <label>
+        <span>User name</span>
+        <br />
+        <Field type="text" name="userName" placeholder="name" required />
+        <ErrorMessage component="p" name="userName" />
+      </label>
+      <br />
+        <span>favourite color</span>
         <br />
         <label>
-          <span>user name</span>
-          <br />
-          <Field type="name" name="userName" placeholder="name" />
-          <ErrorMessage component='p' name="userName" />
+          <Field type="radio" name="favColor" value="red" />
+          <span>Red:</span>
         </label>
-        <br />
-        <button type="submit">new contact</button>
-      </Form>
-    </Formik>
+        <label>
+          <Field type="radio" name="favColor" value="green" />
+          <span>Green:</span>
+        </label>
+        <label>
+          <Field type="radio" name="favColor" value="blue" />
+          <span>Blue:</span>
+          <ErrorMessage component="p" name="favColor" />
+        </label>
+      <br />
+      <button type="submit"> ▶ Create new users</button>
+    </Form>
+  </Formik>
   );
 };
 
--- src/componetns/MailBoxListItem/MailBoxListItem.jsx
+++ src/componetns/MailBoxListItem/MailBoxListItem.jsx
@@ -3,17 +3,10 @@ import css from "./MailBoxListItem.module.css";
 const MailBoxListItem = ({ user, onDeletUser }) => {
   return (
     <li>
-      <p >Email:  {user.userEmail}</p>
-      <p className={css.name}>Name: <div style={{backgroundColor: user.favColor}} className={css.color} /> <b>{user.userName}</b></p>
-
-      <button
-        className={css.btn}
-        type="button"
-        onClick={() => onDeletUser(user.id)}
-      >
-        Delete user
-      </button>
-    </li>
+    <p>Email: {user.userEmail}</p>
+    <p className={css.name}>Name: <span style={{backgroundColor: user.favColor}} className={css.color} /> {user.userName}</p>
+    <button type="button" onClick={() => onDeletUser(user.id)}>❌ Delete user</button>
+  </li>
   );
 };
 
