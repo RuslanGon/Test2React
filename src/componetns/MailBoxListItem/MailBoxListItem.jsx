8585d0e2b474d6638fbc62ab4c8ca09fe9930451
RuslanGon <ruslan441006@gmail.com>
2024-05-12 11:06:13 +0300

    wer

--- src/componetns/MailBoxListItem/MailBoxListItem.jsx
+++ src/componetns/MailBoxListItem/MailBoxListItem.jsx
@@ -1,9 +1,18 @@
+import css from "./MailBoxListItem.module.css";
+
 const MailBoxListItem = ({ user, onDeletUser }) => {
   return (
     <li>
-      <p>Email: {user.userEmail}</p>
-      <p>Name: {user.userName}</p>
-      <button type="button" onClick={() => onDeletUser(user.id)}>Delete user</button>
+      <p >Email:  {user.userEmail}</p>
+      <p className={css.name}>Name: <div style={{backgroundColor: user.favColor}} className={css.color} /> <b>{user.userName}</b></p>
+
+      <button
+        className={css.btn}
+        type="button"
+        onClick={() => onDeletUser(user.id)}
+      >
+        Delete user
+      </button>
     </li>
   );
 };
